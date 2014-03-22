339ef587cd70a8e3f8e10deb9b41501e2efc469e
zdunczyk <tomasz@zdunczyk.org>
2014-03-22 01:35:49 +0100

    [feat] json deep matcher for jasmine

--- tests/jasmine-ext.js
+++ tests/jasmine-ext.js
@@ -12,5 +12,16 @@ function using(name, values, func){
     }
 }
 
+beforeEach(function() {
+    this.addMatchers({
+        jsonEqual: function(expected) {
+            var actual = JSON.stringify(this.actual).replace(/(\\t|\\n)/g, '');
+            expected = JSON.stringify(expected).replace(/(\\t|\\n)/g, '');
+
+            return actual === expected;
+        }
+    });    
+});
+
 if(typeof module !== 'undefined')
     module.exports = using;
