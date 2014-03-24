887828300e05a8263d3ec8d5121e757ebcf48e4d
zdunczyk <tomasz@zdunczyk.org>
2014-03-24 12:50:54 +0100

    [test] integ tests for situations when template doesn't match data stream 1:1

--- tests/integration/unserialize-spec.js
+++ tests/integration/unserialize-spec.js
@@ -7,8 +7,31 @@ describe("Unserialization", function() {
     
     var str = rayson.type.str,
         int32 = rayson.type.int32,
-        bool = rayson.type.bool;
+        bool = rayson.type.bool,
+        int8 = rayson.type.int8;
 
+    var obj1_arr1_data =  {
+        start: true,
+        fields: {
+            a: 'foo',
+            b: 'boo'
+        },
+        arr: [
+            { z: 113, x: 'qwe' },
+            { z: 113, x: 'ewq' }
+        ],
+        end: false
+    };
+
+    function obj1_arr1_provider() {
+        return rayson.serialize(obj1_arr1_data, {
+            root: str,
+            start: bool,
+            end: bool,
+            z: int8
+        });
+    }
+    
     it('should work with simple one-level json', function() {
         var data = {
             foo1: 'bar1',
@@ -60,4 +83,54 @@ describe("Unserialization", function() {
         
         expect(unserialized).jsonEqual(data); 
     });
+
+    it('works when template has some additional fields', function() {
+
+        var template = {
+            start: bool,
+            fields: {
+                a: str, b: str, c: int32
+            },
+            arr: [ 
+                { z: int8, x: str, y: bool }
+            ],
+            end: bool,
+            additional: str
+        };
+        
+        var serialized = obj1_arr1_provider(),
+            unserialized = rayson.unserialize(serialized, template);
+        
+        expect(unserialized).jsonEqual(obj1_arr1_data); 
+    });
+
+    it('works when there are some additional fields in data stream', function() {
+        
+        var template = {
+            start: bool,
+            fields: {
+                a: str 
+            },
+            arr: [ 
+                { z: int8 }
+            ]
+        };
+
+        var result = {
+            start: obj1_arr1_data.start,
+            fields: {
+                a: obj1_arr1_data.fields.a
+            },
+            arr: [
+                { z: obj1_arr1_data.arr[0].z }, 
+                { z: obj1_arr1_data.arr[1].z }
+            ]
+        };
+        
+        var serialized = obj1_arr1_provider(),       
+            unserialized = rayson.unserialize(serialized, template);
+        
+        expect(unserialized).jsonEqual(result); 
+    });
+
 });
